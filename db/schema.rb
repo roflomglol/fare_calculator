5d54af9ec6c1b7b3517755b27c24248b67bfe93f
Sergey Yazan <sergey.yazan@gmail.com>
2017-11-03 14:41:06 +0300

    adds city model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103112630) do
+ActiveRecord::Schema.define(version: 20171103113943) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "fares", force: :cascade do |t|
     t.integer "kind"
