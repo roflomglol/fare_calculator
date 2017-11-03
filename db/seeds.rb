f257bfc74f499341d9a6001e36a3b7fab4830cde
Sergey Yazan <sergey.yazan@gmail.com>
2017-11-03 15:02:19 +0300

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+city = City.find_or_create_by(name: 'Moscow')
+
+fares = {
+  business: {
+    pickup: 250,
+    per_minute: 20,
+    per_km: 20,
+    minimum_price: 500
+  },
+  vip: {
+    pickup: 500,
+    per_minute: 40,
+    per_km: 40,
+    minimum_price: 1000
+  },
+  luxe: {
+    pickup: 450,
+    per_minute: 32,
+    per_km: 80,
+    minimum_price: 2000
+  },
+  plus: {
+    pickup: 375,
+    per_minute: 30,
+    per_km: 30,
+    minimum_price: 2500
+  },
+  kids: {
+    pickup: 450,
+    per_minute: 20,
+    per_km: 20,
+    minimum_price: 700
+  },
+  designated_driver: {
+    pickup: 300,
+    per_minute: 30,
+    per_km: 75,
+    minimum_price: 2000
+  }
+}
+
+fares.each do |kind, params|
+  Fare.create_with(params.merge(city: city)).find_or_create_by(kind: kind)
+end
