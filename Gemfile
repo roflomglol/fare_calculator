0a00a8287e4857c1d5613c653b0a2c0c78c163d5
Sergey Yazan <sergey.yazan@gmail.com>
2017-11-03 17:49:46 +0300

    adds grpc service for estimating distance and time between points

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'rails', '~> 5.1.4'
 gem 'sqlite3'
 gem 'puma', '~> 3.7'
 
+gem 'grpc', '~> 1.0'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
---
+++ app/services/estimator/calculate_time_and_distance_between_points.rb
@@ -0,0 +1,28 @@
+require 'estimator_services_pb'
+
+module Estimator
+  class CalculateTimeAndDistanceBetweenPoints
+    attr_reader :origin, :destination
+
+    def initialize(origin:, destination:)
+      @origin = origin
+      @destination = destination
+    end
+
+    def call
+      stub = Estimator::Estimate::Stub.new('localhost:3001', :this_channel_is_insecure)
+      response = stub.time_and_distance_between_points(message)
+
+      { time: response.time, distance: response.distance }
+    end
+
+    private
+
+    def message
+      origin_message = Origin.new(lat: origin[0], long: origin[1])
+      destination_message = Destination.new(lat: destination[0], long: destination[1])
+
+      PointsInfo.new(origin: origin_message, destination: destination_message)
+    end
+  end
+end
